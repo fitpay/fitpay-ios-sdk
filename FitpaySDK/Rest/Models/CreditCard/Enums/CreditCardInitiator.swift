1fdebd998fe4c26df471adb279a8e2a61c808914
Todd Atkins <29870013+climbonbelay@users.noreply.github.com>
2018-08-21 13:14:29 -0600

    added unknown case

--- FitpaySDK/Rest/Models/CreditCard/Enums/CreditCardInitiator.swift
+++ FitpaySDK/Rest/Models/CreditCard/Enums/CreditCardInitiator.swift
@@ -1,10 +1,12 @@
 import Foundation
 
-/// Identifies the party initiating the deactivation/reactivation request
+/// Identifies the party initiating the lifecycle event
 ///
 /// - cardholder: card holder
 /// - issuer: issuer
+/// - unknown: unknown
 public enum CreditCardInitiator: String, Codable {
     case cardholder = "CARDHOLDER"
     case issuer     = "ISSUER"
+    case unknown    = "UNKNOWN"
 }
