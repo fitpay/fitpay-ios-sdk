e83399cd3584e28c503fbd31da841ddcc4a1b7ee
Jeremiah Harris <jeremiah@fit-pay.com>
2018-10-05 11:20:59 -0600

    abort on merge conflict

--- deploy.sh
+++ deploy.sh
@@ -40,6 +40,16 @@ git push
 git checkout master
 git pull
 git merge develop  -m "v$2 merge development"
+
+# check for conflicts
+CONFLICTS=$(git ls-files -u | wc -l)
+if [ "$CONFLICTS" -gt 0 ] ; then
+    echo "There is a merge conflict. Aborting"
+    git merge --abort
+    exit 1
+fi
+
+# push
 git push
 
 # create tag
