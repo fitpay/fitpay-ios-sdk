9b01fff9286c708e44d6a3ffb23162b67a10f3bc
Jeremiah Harris <jeremiah@fit-pay.com>
2018-10-19 11:03:48 -0600

    card art image fix

--- FitpaySDK/PaymentDevice/PaymentDeviceConnectors/Hendricks/Models/HendricksCreditCard.swift
+++ FitpaySDK/PaymentDevice/PaymentDeviceConnectors/Hendricks/Models/HendricksCreditCard.swift
@@ -118,93 +118,99 @@ public class HendricksCard: HendricksObject {
     private func processCreditCardImage(_ creditCard: FitpaySDK.CreditCard, completion: @escaping (_ data: Data) -> Void) {
         let defaultCardWidth = 200
         let defaultCardHeight = 126
-        let cardImage = creditCard.cardMetaData?.cardBackgroundCombined?.first
+        guard let cardImage = creditCard.cardMetaData?.cardBackgroundCombined?.first else {
+            completion(processPixelData(image: UIImage(named: "mastercard")!))
+            return
+        }
         
-        cardImage?.retrieveAssetWith(options: [ImageAssetOption.width(defaultCardWidth), ImageAssetOption.height(defaultCardHeight), ImageAssetOption.fontScale(20), ImageAssetOption.fontBold(false)]) { (asset, _) in
+        cardImage.retrieveAssetWith(options: [ImageAssetOption.width(defaultCardWidth), ImageAssetOption.height(defaultCardHeight), ImageAssetOption.fontScale(20), ImageAssetOption.fontBold(false)]) { (asset, _) in
             guard let image = asset?.image ?? UIImage(named: "mastercard") else {
                 completion(Data())
                 return
             }
             
-            let pixelData = image.pixelData()!
-            
-            // determine if there is tranparency
-            var transparency = true // false - no transparency currently not working
-            
-            for i in stride(from: 0, to: pixelData.count, by: 4) {
-                let a = pixelData[i + 3]
-                if a < 255 {
-                    transparency = true
-                    break
-                }
-            }
-            
-            // create main data
-            var previousColor: (color: UInt16, alpha: UInt16)?
-            var mainData = Data()
-            var pixelCounter: UInt16 = 0
-            let maxPixelCount = transparency ? 15 : 255
-            
-            for i in stride(from: 0, to: pixelData.count, by: 4) {
-                let r = UInt16(pixelData[i]) & 0xF0
-                let g = UInt16(pixelData[i + 1]) & 0xF0
-                let b = UInt16(pixelData[i + 2]) & 0xF0
-                let a = UInt16(pixelData[i + 3])
-                
-                let red =   ((31 * (r + 4)) / 255)
-                let green = ((63 * (g + 2)) / 255)
-                let blue =  ((31 * (b + 4)) / 255)
-                let alpha = (((15 * (a + 8)) / 255) & 0x0F)
-                
-                var color: UInt16 = (red << 11) | (green << 5) | blue
-                
-                if i == 0 { // handle first case differently
-                    previousColor = (color: color, alpha: alpha)
-                } else {
-                    pixelCounter += 1
-                }
-                
-                if alpha == 0 { // if fully transparent wipe color
-                    color = 0
-                }
-                
-                let lastPixel = i + 4 == pixelData.count
-                
-                if (color: color, alpha: alpha) != previousColor! || pixelCounter >= maxPixelCount || lastPixel {
-                    if !lastPixel {
-                        pixelCounter -= 1
-                    }
-                    
-                    if transparency {
-                        let pixelPlusAlpha: UInt8 = (UInt8(pixelCounter) << 4) | (UInt8(previousColor!.alpha))
-                        mainData += pixelPlusAlpha.data + previousColor!.color.data
-                        
-                    } else {
-                        mainData += pixelCounter.data + previousColor!.color.data
-                    }
-                    
-                    pixelCounter = 0
-                }
-                
-                previousColor = (color: color, alpha: alpha)
-                
-            }
-            
-            // header
-            let imageVersion: UInt8 = 0x41
-            let imageMode: UInt8 = transparency ? 0x01 : 0x00
-            var width = Int(image.size.width)
-            let widthData = Data(bytes: &width, count: 2)
-            var height = Int(image.size.height)
-            let heightData = Data(bytes: &height, count: 2)
-            
-            var mainDataSize = mainData.count
-            let mainDataSizeData = Data(bytes: &mainDataSize, count: 2)
-            
-            let imageHeader = imageVersion.data + imageMode.data + widthData + heightData + mainDataSizeData
-            
-            completion(imageHeader + mainData)
+            completion(self.processPixelData(image: image))
         }
     }
     
+    private func processPixelData(image: UIImage) -> Data {
+        let pixelData = image.pixelData()!
+        
+        // determine if there is tranparency
+        var transparency = true // false - no transparency currently not working
+        
+        for i in stride(from: 0, to: pixelData.count, by: 4) {
+            let a = pixelData[i + 3]
+            if a < 255 {
+                transparency = true
+                break
+            }
+        }
+        
+        // create main data
+        var previousColor: (color: UInt16, alpha: UInt16)?
+        var mainData = Data()
+        var pixelCounter: UInt16 = 0
+        let maxPixelCount = transparency ? 15 : 255
+        
+        for i in stride(from: 0, to: pixelData.count, by: 4) {
+            let r = UInt16(pixelData[i]) & 0xF0
+            let g = UInt16(pixelData[i + 1]) & 0xF0
+            let b = UInt16(pixelData[i + 2]) & 0xF0
+            let a = UInt16(pixelData[i + 3])
+            
+            let red =   ((31 * (r + 4)) / 255)
+            let green = ((63 * (g + 2)) / 255)
+            let blue =  ((31 * (b + 4)) / 255)
+            let alpha = (((15 * (a + 8)) / 255) & 0x0F)
+            
+            var color: UInt16 = (red << 11) | (green << 5) | blue
+            
+            if i == 0 { // handle first case differently
+                previousColor = (color: color, alpha: alpha)
+            } else {
+                pixelCounter += 1
+            }
+            
+            if alpha == 0 { // if fully transparent wipe color
+                color = 0
+            }
+            
+            let lastPixel = i + 4 == pixelData.count
+            
+            if (color: color, alpha: alpha) != previousColor! || pixelCounter >= maxPixelCount || lastPixel {
+                if !lastPixel {
+                    pixelCounter -= 1
+                }
+                
+                if transparency {
+                    let pixelPlusAlpha: UInt8 = (UInt8(pixelCounter) << 4) | (UInt8(previousColor!.alpha))
+                    mainData += pixelPlusAlpha.data + previousColor!.color.data
+                    
+                } else {
+                    mainData += pixelCounter.data + previousColor!.color.data
+                }
+                
+                pixelCounter = 0
+            }
+            
+            previousColor = (color: color, alpha: alpha)
+            
+        }
+        
+        // header
+        let imageVersion: UInt8 = 0x41
+        let imageMode: UInt8 = transparency ? 0x01 : 0x00
+        var width = Int(image.size.width)
+        let widthData = Data(bytes: &width, count: 2)
+        var height = Int(image.size.height)
+        let heightData = Data(bytes: &height, count: 2)
+        
+        var mainDataSize = mainData.count
+        let mainDataSizeData = Data(bytes: &mainDataSize, count: 2)
+        
+        let imageHeader = imageVersion.data + imageMode.data + widthData + heightData + mainDataSizeData
+        
+        return imageHeader + mainData
+    }
 }
