62d23dcf0a13a7fc570a3408c754e9db2c3ac48e
Jeremiah Harris <jeremiah@fit-pay.com>
2018-10-31 12:54:22 -0600

    Make StreamEventType optional to be able to receive all events

--- FitpaySDK/Notifications/EventStream/StreamEvent.swift
+++ FitpaySDK/Notifications/EventStream/StreamEvent.swift
@@ -1,13 +1,14 @@
 import Foundation
 
+// TODO: Replace with EventModel for SDK 2.x
 public class StreamEvent: Decodable {
-    public var type: StreamEventType
+    public var type: StreamEventType?
     public var payload: [String: Any]?
 
     public required init(from decoder: Decoder) throws {
         let container =  try decoder.container(keyedBy: CodingKeys.self)
         
-        type = try container.decode(StreamEventType.self, forKey: .type)
+        type = try? container.decode(.type)
         payload = try? container.decode([String: Any].self, forKey: .payload)
     }
     
