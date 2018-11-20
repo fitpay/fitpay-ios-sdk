8bc17eaaaecbbc2da2cb687093ec69fc5dabce5d
Jeremiah Harris <jeremiah@fit-pay.com>
2018-11-20 09:44:02 -0600

    added ability to close EventSource
    - added unsubscribe method to manager
    - restricted to only one stream per user open at a time

--- FitpaySDK/Notifications/EventStream/UserEventStream.swift
+++ FitpaySDK/Notifications/EventStream/UserEventStream.swift
@@ -3,22 +3,29 @@ import Alamofire
 
 class UserEventStream {
     
+    private var eventSource: EventSource?
+    
     init(user: User, client: RestClient, completion: @escaping UserEventStreamManager.userEventStreamHandler) {
         guard let eventStreamLink = user.eventStreamLink?.href else { return }
         let jsonDecoder = JSONDecoder()
         
         client.prepareAuthAndKeyHeaders { (headers, error) in
-            let eventSource = EventSource(url: eventStreamLink, headers: [:])
-            eventSource.onOpen {
+            self.eventSource = EventSource(url: eventStreamLink, headers: [:])
+            
+            self.eventSource!.onOpen {
                 log.debug("USER_EVENT_STREAM: connected to event stream for user \(user.id ?? "no user")")
             }
             
-            eventSource.onError { (error) in
+            self.eventSource!.onError { (error) in
                 guard let error = error else { return }
-                log.error("USER_EVENT_STREAM: error in event stream: \(error)")
+                if error.code == -999 { //cancelled
+                    log.debug("USER_EVENT_STREAM: connection closed for user \(user.id ?? "no user")")
+                } else {
+                    log.error("USER_EVENT_STREAM: error in event stream: \(error)")
+                }
             }
             
-            eventSource.onMessage { (_, _, data) in
+            self.eventSource!.onMessage { (_, _, data) in
                 guard let jwtBodyString = JWE.decryptSigned(data, expectedKeyId: client.key?.keyId, secret: client.secret) else { return }
                 guard let streamEvent = try? jsonDecoder.decode(StreamEvent.self, from: jwtBodyString.data(using: String.Encoding.utf8)!) else { return }
                 
@@ -28,6 +35,10 @@ class UserEventStream {
                 completion(streamEvent)
             }
         }
+    }
+    
+    func close() {
+        eventSource?.close()
     }
     
 }
