1d54ba108dab64bb3f8e00a4b7bffe03c0f77758
Jeremiah Harris <jeremiah@fit-pay.com>
2018-12-03 11:19:59 -0700

    deleting unneeded code

--- FitpaySDK/Rest/Models/Country/Province.swift
+++ FitpaySDK/Rest/Models/Country/Province.swift
@@ -4,28 +4,5 @@ public class Province: Serializable {
     
     public var name: String?
     public var iso: String?
-    
-    private static let provincesResourceKey = "provinces"
-    
-    private enum CodingKeys: String, CodingKey {
-        case name
-        case iso
-    }
-    
-    // MARK: - Lifecycle
-    
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        name = try? container.decode(.name)
-        iso = try? container.decode(.iso)
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try? container.encode(name, forKey: .name)
-        try? container.encode(iso, forKey: .iso)
-    }
 
 }
